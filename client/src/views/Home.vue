930bea74be8cbdfe9172e86bce782a501c42107d
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-10-27 16:13:28 -0400

    added vue Client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    let ascript = document.createElement('script')
+      ascript.setAttribute('src', '../app.js')
+      document.head.appendChild(ascript)
+
+  },
+  methods: {
+
+  }
+}
+</script>
--- server/server.js
+++ server/server.js
@@ -2,10 +2,24 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT 
+const morgan = require('morgan')
+const path = require('path')
 
-app.use(express.static("static"))
+app.set('view engine', 'ejs')
+//middleware and static files
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "static")));
+//app.use(express.static("static"))
+//app.use(express.urlencoded({ extended: true }))
+//.use(morgan('tiny'))
+
+//routes
 app.get('/', (req, res) => {
-    res.send('Hello Program')
-})
+    res.render('pages/index.ejs')
+  })
+  app.get('/login', (req, res) => {
+    res.render('pages/login.ejs')
+  })
+  
 
 app.listen(PORT, () => console.log(`App listening at http:localhost:${PORT} ..`))
