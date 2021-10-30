0ff8c8972941169f084d11ff9509299bb28c4a76
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-10-30 17:58:06 -0400

    "more changes"

--- server/server.js
+++ server/server.js
@@ -2,13 +2,27 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT 
+const MONGO_URI = process.env.MONGO_URI
+const mongoose = require('mongoose')
+const cors = require('cors')
 const morgan = require('morgan')
 const path = require('path')
-
+const userRoutes = require('./routes/api/users')
 
 //middleware and static files
-//app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.urlencoded({ extended: true }))
 //app.use(express.static(path.join(__dirname, "static")));
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB database connected ...'))
+    .catch((err) => console.log(err))
+
+app.use('/api/users', userRoutes)
 
 //app.use(express.static("static"))
 
