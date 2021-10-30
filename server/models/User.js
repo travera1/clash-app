2d14f4fa9f6b85322fca49e8ec81b52bb92c4b79
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-10-30 17:58:33 -0400

    "more"

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose")
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true, 
+        
+    },
+    lastName: {
+        type: String,
+        required: true, 
+        
+    },
+    username: {
+        type: String,
+        required: true, 
+        
+    },
+    password: {
+        type: String,
+        required: true, 
+        
+    },
+    favorites: {
+        type: {}
+    },
+    date: {
+        type: Date, 
+        default: Date.now,
+    }
+})
+
+const User = model('user', UserSchema)
+
+module.exports = User
+
